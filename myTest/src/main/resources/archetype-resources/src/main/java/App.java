1f052f13a3b7c90b90a872e1665243f4de764b26
Murzabek Suyundikov <murzabek1995@gmail.com>
2022-09-06 16:09:27 +0300

    1.1.3

---
+++ myTest/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.Murzabek.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
